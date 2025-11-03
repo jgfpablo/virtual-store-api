ec6e0333ad4a07c962354d4e6800503bc7f9bb78
Pablo Frank <jgfpablo@gmail.com>
2025-11-03 05:31:07 -0300

    cambios

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,11 +1,9 @@
 import multer from "multer";
 
-// Usamos almacenamiento en memoria para enviar directamente a Cloudinary
+// Usamos almacenamiento en memoria para subir a Cloudinary desde buffer
 const storage = multer.memoryStorage();
 
 export const upload = multer({
     storage,
-    limits: {
-        fileSize: 5 * 1024 * 1024, // 5MB por archivo (ajustable)
-    },
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB por archivo
 });
